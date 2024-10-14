82e76a539432385e394f665255f742248b841e99
türkay ürkmez <turkay.urkmez@outlook.com>
2024-10-14 13:46:09 +0300

    Bind VS Get for reading configure

--- ReadFromConfig/ReadFromConfig/Controllers/HomeController.cs
+++ ReadFromConfig/ReadFromConfig/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ReadFromConfig.Models;
 using System.Diagnostics;
 
@@ -6,21 +6,48 @@ namespace ReadFromConfig.Controllers
 {
     public class HomeController : Controller
     {
+
+        /*
+         * 1. Bind: Bir abstract sınıfın concrete olarak kullanılmasına izin verir.
+         * 2. Get: doğrudan bir instance oluşturur.
+         * 
+         */
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private IConfiguration configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var mailSettings = new EmailOptions("a@b.com");
+            //1. Bind fonksiyonu: appsettings dosyasından değeri oku ve -> instance'a bind et
+            configuration.GetSection(EmailOptions.MailConfigurationName).Bind(mailSettings);
+
+            string content = $"Host: {mailSettings.Host}, Eposta:{mailSettings.Email}";
+            
+            return Content(content);
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            EmailOptions mailSettings = configuration.GetSection(EmailOptions.MailConfigurationName).Get<EmailOptions>();
+            string content = $"Host: {mailSettings.Host}, Eposta:{mailSettings.Email} ";
+            return Content(content);
+
+
+        }
+
+        public IActionResult Sample()
+        {
+            var mailSettings = new EmailOptions("x@y.com");
+            configuration.GetSection(EmailOptions.MailConfigurationName).Bind(mailSettings);
+            var content = $"Host:{mailSettings.Host} Email:{mailSettings.Email}, Pass: {mailSettings.Password}";
+            return Content(content);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ ReadFromConfig/ReadFromConfig/Models/EmailOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace ReadFromConfig.Models
+{
+
+    public abstract class EmailBase
+    {
+        public abstract string Host { get; set; }
+        public abstract string Password { get; set; }
+    }
+    public class EmailOptions : EmailBase
+    {
+
+        public EmailOptions(string email)
+        {
+            Email = email;
+
+        }
+        public const string MailConfigurationName = "MailSettings";
+        public override string Host { get; set; }
+        public override string Password { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public bool EnableSSL { get; set; } = true;
+    }
+}
