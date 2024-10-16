f20412cde57a9b198b6fe5775de6770e1a7290a1
türkay ürkmez <turkay.urkmez@outlook.com>
2024-10-16 12:36:13 +0300

    last check

---
+++ minimalAPIs/RouteHandlers/ProductEndpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace RouteHandlers
+{
+    public static class ProductEndpoints
+    {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("/products/all", () => Results.Ok(new { messagee = "Farz edin ki burada ürün listesi var" })
+                      ).WithName("Products");
+
+            app.MapGet("/links", (LinkGenerator generator) => $"Tüm ürünleri almak için gereken link: {generator.GetPathByName("Products", values: null)}");
+                         
+            app.MapPost("/", () => Results.Created("created/1", new { message = "Eklenen eleman...." }));
+        }
+
+
+    }
+}
--- minimalAPIs/RouteHandlers/Program.cs
+++ minimalAPIs/RouteHandlers/Program.cs
@@ -1,3 +1,5 @@
+﻿using RouteHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//app.MapGet("/", () => "Bu bir get request'tir");
+//app.MapMethods("/deneme", new[] { "PATCH", "HEAD" }, () => "Hem patch hem de head çalışır");
+
+ProductEndpoints.Map(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
